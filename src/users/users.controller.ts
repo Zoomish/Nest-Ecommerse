17c665722da4e8e8827752a46184a42878f2730b
Zoom <egor147536987@gmail.com>
2024-04-23 14:45:32 +0300

    "Remove unnecessary imports and guards from UsersController"
    This commit removes the following imports from the UsersController file:
    
    * @nestjs/common: ValidationPipe, UsePipes
    * @nestjs/

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,8 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseGuards,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles-auth.decorator';
 import { AddRoleDto } from './dto/add-role.dto';
