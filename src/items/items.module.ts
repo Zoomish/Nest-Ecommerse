720ffd42b65a18b4edbc3b9e39f4342880a0d39a
Zoom <egor147536987@gmail.com>
2024-04-23 18:06:10 +0300

    "Import CategoriesModule in ItemsModule"
    This commit adds the `CategoriesModule` to the imports array in the `ItemsModule`. This could potentially indicate that the `ItemsService` or `ItemsController` now depend on functionality

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -5,10 +5,15 @@ import { Item } from './items.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { FilesModule } from 'src/files/files.module';
 import { Category } from 'src/category/category.model';
+import { CategoriesModule } from 'src/category/category.module';
 
 @Module({
   providers: [ItemsService],
   controllers: [ItemsController],
-  imports: [SequelizeModule.forFeature([Item, Category]), FilesModule],
+  imports: [
+    SequelizeModule.forFeature([Item, Category]),
+    FilesModule,
+    CategoriesModule,
+  ],
 })
 export class ItemsModule {}
