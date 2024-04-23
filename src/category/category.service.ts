5a420d5103fc7810d6233e0cb27103989c8d9e5b
Zoom <egor147536987@gmail.com>
2024-04-23 16:12:16 +0300

    Remove unused dependency `RolesService` in CategoriesService
    This commit removes the import statement and private property for `RolesService` in `CategoriesService` as it is not being used in the file. This reduces unnecessary dependencies

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Category } from './category.model';
 import { InjectModel } from '@nestjs/sequelize';
-import { RolesService } from '../roles/roles.service';
 
 @Injectable()
 export class CategoriesService {
   constructor(
     @InjectModel(Category) private categoryRepository: typeof Category,
-    private roleService: RolesService,
   ) {}
 
   async getAllCategories() {
