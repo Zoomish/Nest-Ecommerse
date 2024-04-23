e33e90e421f706f671195865236813e687c58eb4
Zoom <egor147536987@gmail.com>
2024-04-23 17:36:09 +0300

    "Added file upload interceptor to createCategory method in CategoriesController"
    The diff includes changes to the CategoriesController file in a Nest.js project. Specifically, it adds the `@UseInterceptors(FileInter

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CategoriesService } from './category.service';
 import {
   ApiBearerAuth,
@@ -9,11 +16,18 @@ import {
 import { User } from 'src/users/users.model';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles-auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @ApiTags('Категории')
 @Controller('categories')
 export class CategoriesController {
   constructor(private categoryService: CategoriesService) {}
+
+  @UseInterceptors(FileInterceptor('image'))
+  createItem(@Body() dto: CreateCategoryDto, @UploadedFile() image) {
+    return this.categoryService.createCategory(dto, image);
+  }
 
   @ApiOperation({ summary: 'Получить всех пользователей' })
   @ApiResponse({ status: 200, type: [User] })
