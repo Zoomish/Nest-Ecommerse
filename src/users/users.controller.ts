57775cf3517a02063161cf1ad75fb26d7cd422db
Zoom <egor147536987@gmail.com>
2024-04-23 15:07:24 +0300

    "Add create user functionality to UsersController and UsersService"
    This diff adds the ability to create a new user in the users controller and service. A new route has been added to the UsersController to accept a CreateUserDto object and pass

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
@@ -11,6 +12,14 @@ import { BanUserDto } from './dto/ban-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @ApiOperation({ summary: 'Создание пользователя' })
+  @ApiResponse({ status: 200, type: User })
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.userService.createUser(userDto);
+  }
+
   @ApiOperation({ summary: 'Получить всех пользователей' })
   @ApiResponse({ status: 200, type: [User] })
   @Roles('ADMIN')
