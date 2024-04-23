20a05c1c2ec589b104bae905c854ec12882f2a44
Zoom <egor147536987@gmail.com>
2024-04-23 17:31:32 +0300

    "Remove validation for password field in CreateCategoryDto"
    Explanation:
    The code diff shows that the `Length` validation for the `password` field has been removed in the `CreateCategoryDto` class. The `IsString

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,13 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Length } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty({ example: 'test@gmail.com', description: 'Почтовый адрес' })
   @IsString({ message: 'Должно быть строкой' })
   readonly title: string;
-
-  @ApiProperty({ example: '12345', description: 'Пароль пользователя' })
-  @IsString({ message: 'Должно быть строкой' })
-  @Length(4, 16, { message: 'От 4 до 16 символов' })
-  readonly password: string;
 }
