479f2f2a57c64e2081726e7a96bd891c30684a45
Zoom <egor147536987@gmail.com>
2024-04-23 15:35:56 +0300

    Remove CreateRoleDto from roles module
    - Removed CreateRoleDto from the roles module
    - Deleted related imports and usages in roles controller and service
    - Removed ApiProperty decorators and response types from roles controller methods

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,20 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { RolesService } from './roles.service';
-import { CreateRoleDto } from './dto/create-role.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Role } from './roles.model';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
   constructor(private roleService: RolesService) {}
-
-  @ApiOperation({ summary: 'Создание роли' })
-  @ApiResponse({ status: 200, type: Role })
-  @Post()
-  create(@Body() dto: CreateRoleDto) {
-    return this.roleService.createRole(dto);
-  }
 
   @Get('/:value')
   getByValue(@Param('value') value: string) {
