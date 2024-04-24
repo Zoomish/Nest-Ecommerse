71b364ce8ff7a2c67c5a7a44e1e0408989be3d61
Zoom <egor147536987@gmail.com>
2024-04-24 21:51:45 +0300

    Remove UsersController for users endpoint restrictions
    The UsersController has been removed from the users module.
    The UsersService is still available for user-related functionality.
    The endpoint restrictions and authorization have been removed as a result of this change.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,4 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.model';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -9,7 +8,6 @@ import { AuthModule } from 'src/auth/auth.module';
 import { UserRoles } from 'src/roles/user-roles.model';
 
 @Module({
-  controllers: [UsersController],
   providers: [UsersService],
   imports: [
     SequelizeModule.forFeature([User, Role, UserRoles]),
