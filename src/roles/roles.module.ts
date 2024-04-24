47ba6b4cc58cff81af1fb32441a936b1104f6a75
Zoom <egor147536987@gmail.com>
2024-04-24 21:52:28 +0300

    Remove roles controller
    The roles controller and its methods have been removed from the project. The file `roles.controller.ts` was deleted and the corresponding import statement was removed from `roles.module.ts`.

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from './roles.model';
 import { User } from '../users/users.model';
@@ -8,7 +7,6 @@ import { UserRoles } from './user-roles.model';
 
 @Module({
   providers: [RolesService],
-  controllers: [RolesController],
   imports: [SequelizeModule.forFeature([Role, User, UserRoles])],
   exports: [RolesService],
 })
