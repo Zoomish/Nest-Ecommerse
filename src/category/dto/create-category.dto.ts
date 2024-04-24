8b6db47e64f42687ddc1d00a809eb0f55a806d43
Zoom <egor147536987@gmail.com>
2024-04-24 21:36:20 +0300

    "Updated CreateCategoryDto title property description"
    The diff shows that in the `CreateCategoryDto` class, the `@ApiProperty` decorator for the `title` property has been updated to change the example value from an email address

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
-  @ApiProperty({ example: 'test@gmail.com', description: 'Почтовый адрес' })
+  @ApiProperty({ example: 'Саке', description: 'Заголовок категории' })
   @IsString({ message: 'Должно быть строкой' })
   readonly title: string;
 }
