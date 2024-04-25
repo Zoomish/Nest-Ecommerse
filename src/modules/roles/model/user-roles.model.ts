1e3b89a7f40a1bd1c075745b8413922cae03ccf3
Zoom <egor147536987@gmail.com>
2024-04-25 18:44:18 +0300

    Update User import path in UserRoles model
    Change the import path of the User model from 'src/users/model/users.model' to 'src/modules/users/model/users.model' in the UserRoles

--- src/modules/roles/model/user-roles.model.ts
+++ src/modules/roles/model/user-roles.model.ts
@@ -5,7 +5,7 @@ import {
   Model,
   Table,
 } from 'sequelize-typescript';
-import { User } from 'src/users/model/users.model';
+import { User } from 'src/modules/users/model/users.model';
 import { Role } from './roles.model';
 
 @Table({ tableName: 'user-roles', timestamps: false })
