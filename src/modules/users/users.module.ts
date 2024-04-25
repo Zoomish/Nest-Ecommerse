8fac1d176862ef14b6d1f2ace00e1906a86fa58e
Zoom <egor147536987@gmail.com>
2024-04-25 18:43:11 +0300

    "Updated imports for RolesModule and Role in users.module.ts"
    Explanation:
    The code diff shows that there are changes in the import statements related to the RolesModule and Role in the users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,8 +2,8 @@ import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './model/users.model';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Role } from 'src/roles/model/roles.model';
-import { RolesModule } from 'src/modules/roles/models/roles.model';
+import { Role } from 'src/modules/roles/model/roles.model';
+import { RolesModule } from 'src/modules/roles/model/user-roles.model';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { UserRoles } from 'src/modules/roles/model/user-roles.model';
 
