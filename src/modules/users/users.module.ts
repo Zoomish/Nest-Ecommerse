9d053864bca623fa8f62757b26e5f46a121de09b
Zoom <egor147536987@gmail.com>
2024-04-25 18:44:29 +0300

    Update import statement for RolesModule in users.module.ts
    - Change import path for RolesModule
    - Import 'RolesModule' from 'src/modules/roles/roles.module' instead of 'src/

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { User } from './model/users.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from 'src/modules/roles/model/roles.model';
-import { RolesModule } from 'src/modules/roles/model/user-roles.model';
+import { RolesModule } from 'src/modules/roles/roles.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { UserRoles } from 'src/modules/roles/model/user-roles.model';
 
